976b78fb87fc504b006fd1ce1e2b066703251684
Daniel Curran <danielcurran@Daniels-MacBook-Pro.local>
2016-04-19 11:00:21 -0500

    using break and continue with for loops

---
+++ break_continue.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Print all evens using for loops and continue\n";
+for ($i=0; $i <= 100; $i++) {
+    if($i % 2 == 1){
+        continue;
+    }
+    echo "{$i}\n";
+}
+
+echo "\n\nBreak Exercise\n";
+for ($x=0; $x <= 100; $x++) { 
+    if($x == 11){
+        break;
+    }
+    echo "{$x}\n";
+}
+?>
